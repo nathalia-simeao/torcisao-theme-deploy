d1dc76ed26685b0a5944416e18b1955b61b61600
Nathalia Simeão <natysandler136@gmail.com>
2026-09-10 13:43:20 -0300

    Load unified Haste form router before quote drawer opens

--- theme/hastebc.php
+++ theme/hastebc.php
@@ -40,6 +40,7 @@ get_template_part('template-parts/haste-family');
   }
 })();
 </script>
+<script defer src="<?php echo esc_url(get_template_directory_uri() . '/assets/torcisao-haste-form-router-v44.js'); ?>?v=20260910-1334"></script>
 <?php if (in_array($torcisao_haste_lang, ['en','es'], true)) : ?>
 <script defer src="<?php echo esc_url(get_template_directory_uri() . '/assets/torcisao-haste-i18n-v42.js'); ?>?v=20260910-1"></script>
 <?php endif; ?>
